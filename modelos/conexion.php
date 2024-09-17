0726af8341c906fa6f7fdd081035ec771add6be3
Jose Luis <jose.vences.xmarts@gmail.com>
2024-09-17 14:07:41 -0600

    [FIX]

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,8 +4,8 @@ class Conexion{
     
     static public function conectar(){
         try {
-            $con = new PDO('mysql:host=localhost;dbname=sistema_de_ropa', 'root', 'admin');
-            // $con = new PDO('mysql:host=localhost;dbname=u442818963_bd_boutiquelor', 'u442818963_jose', '8bA$*/=N');
+            // $con = new PDO('mysql:host=localhost;dbname=sistema_de_ropa', 'root', 'admin');
+            $con = new PDO('mysql:host=localhost;dbname=u442818963_bd_boutiquelor', 'u442818963_jose', '8bA$*/=N');
             return $con;
         } catch (PDOException $e) {
             echo "Fallo la conexion: " . $e->getMessage();
